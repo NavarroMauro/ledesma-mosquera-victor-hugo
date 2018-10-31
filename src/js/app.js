f797ef96cc9f971db55fe86dad9ef658a9542a66
Transluciddata <mcifuent@gmail.com>
2018-10-31 20:40:13 +0100

    adding contact form

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,20 @@
 // JS Goes here - ES6 supported
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+// console.log("🦊 Hello! Edit me in src/js/app.js");
+
+let map = L.map('map').setView([53.550303, 9.992291], 10); 
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+	ext: 'jpg',
+    maxZoom: 13,
+    minZoom: 13,
+    scrollWheelZoom: false,
+    zoomControl: false,
+}).addTo(map);
+
+// L.marker([53.550303, 9.992291]).addTo(map)
+//     .bindPopup('HAMBURG')
+//     .openPopup();
