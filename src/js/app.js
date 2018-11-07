0e446e0116dd0de2d2ade5332731aff25650fc1f
Transluciddata <mcifuent@gmail.com>
2018-11-07 14:56:48 +0100

    starting svg animation

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,33 @@
 // Say hello
 // console.log("🦊 Hello! Edit me in src/js/app.js");
 
-let map = L.map('map').setView([53.550303, 9.992291], 10); 
+// From here start the javascript animation
+
+let names = document.querySelectorAll('svg > g > g > path');
+console.log(names);
+
+
+
+for (let i = 0; i < names.length; i++) {
+    const length = names[i].getTotalLength();
+    names[i].style.strokeDasharray = length + ' ' + length;
+    names[i].style.strokeDashoffset = length;
+    names[i].getBoundingClientRect();
+    names[i].style.transition = 'stroke-dashoffset 2s ease-in-out';
+    names[i].style.strokeDashoffset = '0';
+};
+
+
+
+// From here start the leaflet map
+const map = L.map('map', {
+    scrollWheelZoom: false
+}).setView([53.550303, 9.992291], 10);
 
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: 'abcd',
-	ext: 'jpg',
+    ext: 'jpg',
     maxZoom: 13,
     minZoom: 13,
     scrollWheelZoom: false,
